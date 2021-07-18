7c4be38a64d6401e6f905c3c734e7b32ef73aa10
SethniBhagya <84002137+SethniBhagya@users.noreply.github.com>
2021-07-18 12:25:49 +0530

    Update and rename q1 to q1.scala

---
+++ assignment2/q1.scala
@@ -0,0 +1,21 @@
+import scala.io.StdIn._
+
+object PrimeNo extends App{
+
+  def gcd(x:Int,y:Int):Int=y match{
+    case 0=>x
+    case y if(y>x) =>gcd(y,x)
+    case _ =>gcd(y,x%y)
+  }
+
+  def prime(p:Int,n:Int=2):Boolean= n match{
+    case x if p==x => true
+    case x if gcd(p,x)>1 => false
+    case x  =>prime(p,x+1)
+ }
+  print("Enter a Number: ")
+  var z=readInt()
+
+  println(prime(z,2))
+
+}
