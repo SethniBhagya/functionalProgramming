158f975fa6719ebcd52b4870f1a9bf1d48943680
SethniBhagya <84002137+SethniBhagya@users.noreply.github.com>
2021-07-18 12:46:34 +0530

    Create q2.scala

---
+++ assignment2/q2.scala
@@ -0,0 +1,38 @@
+import scala.util.control.Breaks._  
+import scala.io.StdIn._
+
+
+object PrimeNo extends App{
+
+  def gcd(x:Int,y:Int):Int=y match{
+    case 0=>x
+    case y if(y>x) =>gcd(y,x)
+    case _ =>gcd(y,x%y)
+  }
+
+  def prime(p:Int,n:Int=2):Boolean= n match{
+    case x if p==x => true
+
+    case x if gcd(p,x)>1 => false
+
+    case x  =>prime(p,x+1)
+
+   }
+
+   def printsequeance(x:Int,y:Int){
+    if (x==y){
+        break;
+    }
+    if(prime(y,2)){
+        println(y);
+    }
+    printsequeance(x,y+1);
+   }
+
+
+  print("Enter a Number: ");
+  var z=readInt();
+
+  printsequeance(z,2);
+
+}
