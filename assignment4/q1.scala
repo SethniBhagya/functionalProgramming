939cb19a7452d083e5e7606eba367c8048e3eb01
SethniBhagya <84002137+SethniBhagya@users.noreply.github.com>
2021-08-20 08:51:56 +0530

    Update q1.scala

--- assignment4/q1.scala
+++ assignment4/q1.scala
@@ -1,1 +1,16 @@
+object MyApp extends App{
+	val r1= new Rational(2,3)
+	val x=r1.neg(r1)
+	println(x)
+}
 
+class Rational(m:Int,d:Int){
+	def numer=m
+	def denom = d
+
+	def neg(r:Rational)=new Rational(-this.numer,this.denom){
+
+	}
+
+	override def toString= numer+"/"+denom
+}
